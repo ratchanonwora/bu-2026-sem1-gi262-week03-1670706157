b84c7ccfcd6f1de86ac0426661c30139428d6f57
ratchanonwora <ratchanon.wora@bumail.net>
2026-09-01 10:02:27 +0700

    chore: initialize project settings and global URP configuration assets

--- Assets/Scripts/Workspace/Assignment/StudentSolution/LCT04AccessModifier.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution/LCT04AccessModifier.cs
@@ -55,10 +55,11 @@ namespace Assignment.StudentSolution.LCT04
             // 1. กำหนด specie = "Dog"
             // สามารถเข้าถึง specie ได้เนื่องจาก specie เป็น protected
             // ทำให้สามารถเข้าถึงได้จาก class ที่สืบทอด Animal ได้
-
+            specie = "Dog";
             // 2. กำนด this.name = name ที่รับเป็น parameter จาก constructor
             // สามารถเข้าถึง name ได้เนื่องจาก name เป็น public 
             // ทำให้สามารถเข้าถึงได้จาก class นี้ได้ เนื่องจาก class นี้สืบทอด Animal
+            this.name = name;
 
             // ไม่สามารถเข้าถึง health ได้เนื่องจาก health เป็น private
             // ทำให้ใช้งานได้แค่ใน class ที่ประกาศ health ไว้เท่านั้น ซึ่งก็คือ class Animal
